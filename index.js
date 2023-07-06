a863e1f3990b987a4ccd86a5fb38d58bc9eb6145
graciasvito <vitoristo1@gmail.com>
2023-07-06 07:58:07 +0700

    update dirname

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 require("dotenv").config();
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-// const expressValidator = require("express-validator");
-const flash = require("express-flash");
-const session = require("express-session");
-const bodyParser = require("body-parser");
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+// var expressValidator = require("express-validator");
+var flash = require("express-flash");
+var session = require("express-session");
+var bodyParser = require("body-parser");
 
-// const pg = require("pg");
-// const connection = require("/database");
+// var pg = require("pg");
+// var connection = require("/database");
 
-const indexRouter = require("./src/routes/index");
-const musicRouter = require("./src/routes/music");
+var indexRouter = require("./src/routes/index");
+var musicRouter = require("./src/routes/music");
 
-const app = express();
+var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "/src/views"));
 app.set("view engine", "ejs");
 
 app.use(logger("dev"));
@@ -49,15 +49,15 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
 
 app.listen(port, () => {
   console.log(`Server is Running on port ${port}`);
